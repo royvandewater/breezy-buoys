a04a1c443dc37c1326f975ca49b3e31523df88b4
Roy van de Water <roy.v.water@gmail.com>
2024-07-17 10:34:02 -0400

    Render a "boat", just a triangle for now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-import { Engine, DisplayMode } from "excalibur";
+import {
+  Engine,
+  DisplayMode,
+  Actor,
+  Color,
+  vec,
+  Polygon,
+  PolygonCollider,
+} from "excalibur";
 
 const game = new Engine({
   displayMode: DisplayMode.FillScreen,
 });
 game.start();
+
+const boatPoints = [vec(-20, 20), vec(0, -20), vec(20, 20)];
+const boat = new Actor({
+  pos: vec(game.halfDrawWidth, 100),
+  collider: new PolygonCollider({ points: boatPoints }),
+});
+const boatGraphic = new Polygon({
+  points: boatPoints,
+  color: Color.Green,
+});
+boat.graphics.use(boatGraphic);
+
+game.currentScene.add(boat);
